9d4e8daee6490b65449bfeb42249c9353e4800d3
Chris920 <cdtded1212@gmail.com>
2016-04-22 19:36:06 -0400

    made a basic form blog form that can insert into mongodb

---
+++ imports/api/blogs.js
@@ -0,0 +1,12 @@
+import { Meteor } from 'meteor/meteor';
+import { Mongo } from 'meteor/mongo';
+
+export const Blogs = new Mongo.Collection('blogs');
+
+Meteor.methods({
+    'submitPost'(title, body){
+        console.log(title);
+        console.log(body);
+        Blogs.insert({title: title, body: body});      
+    }
+})
