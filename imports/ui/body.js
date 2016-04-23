258c33ce9a692bf855e45ea3e4772e03a885228e
Chris920 <cdtded1212@gmail.com>
2016-04-23 19:15:45 -0400

    added total blogged count and added styling to the main page

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,11 +1,39 @@
 import { Template } from 'meteor/templating';
 
 import { Blogs } from '../api/blogs.js';
- 
+
+import './blog.js'; 
 import './body.html';
  
 Template.body.helpers({
   blogs() {
-    return Blogs.find({});
+    return Blogs.find({}, {sort: {createdAt: -1}});
+  },
+  blogCount() {
+    return Blogs.find({}).count();
+  }
+});
+
+Template.body.events({
+  'submit #blogForm'(event) {
+    event.preventDefault();
+    const target = event.target;
+    const title = target.title.value;
+    const body = target.body.value;
+    
+    // Meteor.call('submitPost', title, body);
+    // title = '';
+    // body = '';
+    // Insert a blog into the collection
+    Blogs.insert({
+      title,
+      body,
+      createdAt: new Date(), // current time
+    });
+ 
+    // Clear form
+    target.title.value = '';
+    target.body.value = '';
+
   },
 });
