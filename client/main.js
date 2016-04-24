0377d461252ccdcc68299d2fcbcce0b8ede9e0a5
Chris920 <cdtded1212@gmail.com>
2016-04-24 14:20:46 -0400

    this branch has something wrong

--- client/main.js
+++ client/main.js
@@ -1,4 +1,5 @@
-import '../imports/ui/body.js';
+import '../imports/startups/account-config.js';
+// import '../imports/ui/body.js';
 
 // import { Meteor } from 'meteor/meteor';
 // import { Template } from 'meteor/templating';
